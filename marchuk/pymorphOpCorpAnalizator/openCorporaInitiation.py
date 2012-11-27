d19be48a8099216e17cdef84257caa3938e445ee
Amarchuk <aamarchuk@gmail.com>
2012-11-27 22:04:13 +0400

    better DictionaryDivision method and simple findToken

--- marchuk/pymorphOpCorpAnalizator/openCorporaInitiation.py
+++ marchuk/pymorphOpCorpAnalizator/openCorporaInitiation.py
@@ -6,37 +6,50 @@ import sys
 def divideDictionary(path):
     try:
         wholeDictionary = open(''.join([path,'/dict.opcorpora.txt']),'r')
-        hash = 1025
-        dictPart = open(''.join([path,'/',str(hash),'part.dict.opcorpora.txt']),'aw+')
+        hash = []
         newWord = True
+        wordSet = []
         for line in wholeDictionary.readlines():
             line = unicode(line, 'utf-8')
-            if not (ord(line[0]) in range(1024,1106)):
+            if line == '\n':
                 newWord = True
-                dictPart.write(line.encode('utf-8'))
+                for line in wordSet:
+                    if ord(line[0]) in range(1024,1106):
+                        hash.append(ord(line[0]))
+                hash = list(set(hash))
+                for h in hash:
+                    dictPart = open(''.join([path,'/',str(h),'part.dict.opcorpora.txt']),'aw+')
+                    dictPart.write("\n")
+                    for word in wordSet:
+                        try:
+                            dictPart.write(word.encode('utf-8'))
+                        except IOError:
+                            print 'Can`t open file', ''.join([path,'/',str(hash),'part.dict.opcorpora.txt'])
+                    dictPart.close()
+                hash = []
+                wordSet = []
             else:
                 if newWord:
-                    if ord(line[0]) != hash:
-                        try:
-                            dictPart.close()
-                        except IOError:
-                            print 'Can`t close', dictPart.name
-                        hash = ord(line[0])
-                        try:
-                            dictPart = open(''.join([path,'/',str(hash),'part.dict.opcorpora.txt']),'aw+')
-                            #print 'Create file', ''.join([path,'/',str(hash),'part.dict.opcorpora.txt'])
-                            dictPart.write(line.encode('utf-8'))
-                        except IOError:
-                            print 'Can`t create file', ''.join([path,'/',str(hash),'part.dict.opcorpora.txt'])
+                    wordSet.append(line)
                     newWord = False
                 else:
-                    dictPart.write(line.encode('utf-8'))
+                    wordSet.append(line)
+        print 'Finish divide whole dictionary into small parts.'
     except IOError:
         print 'Wrong path to OpenCorp dictionary. '
     finally:
         wholeDictionary.close()
 
+def findToken(token,path):
+    key = ord(token[0])
+    dictionary = open(''.join([path,'/',str(key),'part.dict.opcorpora.txt']),'r+')
+    for line in dictionary.readlines():
+        line = unicode(line, 'utf-8')
+        if token in line:
+            print line
 
-divideDictionary('/home/amarch/Documents/CSCenter/Python')
+
+#divideDictionary('/home/amarch/Documents/CSCenter/Python')
+findToken(u'ЗЕБРА', '/home/amarch/Documents/CSCenter/Python')
 
 
