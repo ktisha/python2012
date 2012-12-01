ddfec115158159c8d7507de4ce28bfe8d2b4d6e0
Ksenia Mamich <fenice@yandex-team.ru>
2012-12-01 12:15:44 +0400

    Additional logging

--- zaikina_mamich/similar-images/similarimages/preview_generator.py
+++ zaikina_mamich/similar-images/similarimages/preview_generator.py
@@ -9,19 +9,23 @@ logger.setLevel(logging.DEBUG)
 
 def generate_preview(image_path, preview_path):
   logger.info('generate_preview() entered')
-  logger.info('Image path: {0}'.format(image_path))
+  logger.debug('Image path: {0}'.format(image_path))
+  logger.debug('Preview path: {0}'.format(preview_path))
+
   image = Image.open(image_path)
-  logger.info('Image size: {0}'.format(image.size))
+  logger.debug('Image size: {0}'.format(image.size))
 
   (max_width, max_height) = (400, 200)
+  logger.debug('Max preview size: w {0}, h {1}'.format(max_width, max_height))
   (real_width, real_height) = image.size
 
-  (coefficient_width, coefficient_height) = (max_width / real_width, max_height / real_height)
+  (coefficient_width, coefficient_height) = (float(max_width) / real_width, float(max_height) / real_height)
+  logger.debug('Coefficients: w {0}, h {1}'.format(coefficient_width, coefficient_height))
   coefficient = coefficient_height if coefficient_width > coefficient_height else coefficient_width
 
-  new_size = (real_width * coefficient, real_height * coefficient)
+  logger.debug('Coefficient: {0}'.format(coefficient))
+  new_size = (int(real_width * coefficient), int(real_height * coefficient))
 
-  logger.info('Preview path: {0}'.format(preview_path))
   logger.info('Preview size: {0}'.format(new_size))
 
   preview = image.resize(new_size, Image.ANTIALIAS)
