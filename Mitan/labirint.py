07e9f6772bb2fcf0e0effc887dcfe7240b76f4d3
Mitan <dmitrykharkovsky@gmail.com>
2012-12-03 01:50:02 +0400

    all done

--- Mitan/labirint.py
+++ Mitan/labirint.py
@@ -1,4 +1,5 @@
 import sys
+
 import pygame
 import string
 
@@ -43,6 +44,7 @@ def draw_a_line(window, color, startx, starty, orientation):
         pygame.draw.line(window, (255, 255, 255), (start_x, start_y), (end_x, end_y), 3)
     elif color == "green":
         pygame.draw.line(window, (0, 255, 0), (start_x, start_y), (end_x, end_y), 3)
+
 def draw_entrance_and_exit(window, st_end, rows):
     """Draws entrance and exit in window"""
     answer = []
@@ -60,13 +62,27 @@ def draw_entrance_and_exit(window, st_end, rows):
         answer.append(cell)
     #in answer we get to ints - ord numbers of entrance and exit cells
     return answer
-def draw_a_circle(window, x, y):
-    """Draws a small circle in the center of cell with coordinates x and y"""
+
+def draw_an_arrow(window, x, y, orientation):
+    """Draws a small arrow in the center of cell with coordinates x and y"""
     global ed_pix
-    center_x = int (round ((x + 1.5) * ed_pix))
-    center_y = int(round ((y + 1.5) * ed_pix))
-    radius = int (round(ed_pix * 0.1))
-    pygame.draw.circle(window, (255, 0, 0), (center_x, center_y), radius)
+    size = int(round(0.5 * ed_pix))
+    s = pygame.Surface((size, size))
+    startx = int(round(size * 0.2))
+    endx = int(round(size * 0.8))
+    starty = int(round(size * 0.5))
+    endy = int(round(size * 0.5))
+    starty2 = int(round(size * 0.4))
+    starty3 = int(round(size * 0.6))
+    startx2 = int(round(size * 0.7))
+    pygame.draw.line(s, (255, 0, 0), (startx, starty), (endx, endy), 1)
+    pygame.draw.line(s, (255, 0, 0), (startx2, starty2), (endx, endy), 1)
+    pygame.draw.line(s, (255, 0, 0), (startx2, starty3), (endx, endy), 1)
+    s = pygame.transform.rotate(s, orientation)
+    screenx = int(round((x + 1.25) * ed_pix))
+    screeny = int(round((y + 1.25) * ed_pix))
+    window.blit(s, (screenx, screeny))
+   
 def create_empty_labirint_graph(cols, rows):
     """Creates graph of a labirints with no walls inside"""
     edges = []
@@ -89,7 +105,36 @@ def create_empty_labirint_graph(cols, rows):
             edges[0:0]  = [[rows * i + j, rows * (i - 1) + j], [rows * i + j, rows * (i + 1) + j]]
     return edges
 
-input_strings = open('input.txt', 'r').readlines()
+def check_wall(window, first, next):
+    """Check if there is a wall between cells first and next"""
+    global cols, rows
+    global walls
+    is_wall = False
+    orientation = 0
+    diff = next - first
+    if [first, next] in walls:
+        startx = next // rows
+        starty = next % rows
+        is_wall = True
+        if diff == 1:
+            draw_a_line(window, "green", startx, starty, 0)
+        elif diff == -1:
+            draw_a_line(window, "green", startx, starty  + 1, 0)
+        elif diff == rows:
+            draw_a_line(window, "green", startx, starty, 1)
+        elif diff == - rows:
+            draw_a_line(window, "green", startx + 1, starty, 1)
+    if diff == 1:
+        orientation = 270
+    elif diff == -1:
+        orientation = 90
+    elif diff == rows:
+        pass
+    elif diff == - rows:
+        orientation = 180
+    return is_wall, orientation
+
+input_strings = open(sys.argv[1], 'r').readlines()
 cr_string = map(int, string.split(input_strings[0]))
 #in first string cols  rows bombs of labirint
 cols, rows, bombs = cr_string
@@ -111,7 +156,7 @@ finish = draw_entrance_and_exit(window, input_strings[1:3], rows)[1]
 edges = create_empty_labirint_graph(cols, rows)
 # walls - list of forbidden edges - between these pairs of cells there are walls
 walls = []
-#in next strings 1-cell edges in format x y orientation
+#in next strings 1-cell walls in format x y orientation
 #x and y are coordinates of cell which is lower or righter from the edge
 #orientation is zero if it's horizontal and 1 if vertical
 for str in input_strings[3:]:
@@ -134,20 +179,45 @@ for edge in edges:
         edges_3d_graph.append([i * cols * rows + edge[0], i * cols * rows + edge[1]])
 for edge in walls:
     for i in range(bombs):
-        edges_3d_graph.append([i * cols * rows + edge[0], (i + 1 ) * cols * rows + edge[1]])
+        edges_3d_graph.append([i * cols * rows + edge[0], (i + 1) * cols * rows + edge[1]])
 # make a new virtual vertice connected with finish cells of all levels
 for i in range(bombs + 1):
     edges_3d_graph.append([i * cols * rows + finish, vertices_3d_graph])
-    edges_3d_graph.append([vertices_3d_graph,i * cols * rows + finish ])
+    edges_3d_graph.append([vertices_3d_graph, i * cols * rows + finish])
 #3d graph of labirint
-Ghraph_3d = [vertices_3d_graph + 1 , edges_3d_graph]
+Ghraph_3d = [vertices_3d_graph + 1, edges_3d_graph]
 #BFS gives as output[2] path from start to finish
-for k in BFS(Ghraph_3d, start, vertices_3d_graph)[2]:
-    k = k % (cols * rows)
-    i = k // rows
-    j = k % rows
-    draw_a_circle(window, i, j)
 
+if BFS(Ghraph_3d, start, vertices_3d_graph)[0]:
+    #path to exit
+    path = map (lambda x: x % (cols * rows), BFS(Ghraph_3d, start, vertices_3d_graph)[2])
+    for k in path:
+        if path.index(k) != len(path) - 1:
+            next = path[path.index(k) + 1]
+            #check if there is a wall and color it with green
+            check_wall(window, k, next)
+            #orientation of way to next cell
+            next_orientation = check_wall(window, k, next)[1]
+            i = k // rows
+            j = k % rows
+            draw_an_arrow(window, i, j, next_orientation)
+    #for finish we need special arrow-orientation
+    finishx  = finish // rows
+    finishy = finish % rows
+    finish_orientation = 0
+    if finishx == 0:
+        finish_orientation = 180
+    elif  finishx == cols - 1:
+        pass
+    elif  finishy == rows - 1:
+        finish_orientation = 270
+    elif  finishy == 0:
+        finish_orientation = 90
+    draw_an_arrow(window, finishx, finishy, finish_orientation)
+else:
+    myfont = pygame.font.SysFont(None, ed_pix)
+    label = myfont.render("NO WAY, DUDE!", 1, (255, 0, 0))
+    window.blit(label, (ed_pix, 0))
 pygame.display.flip()
 
 while True:
