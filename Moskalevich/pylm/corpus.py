c4b591224e4b0392496bbd63abd61429bb70bafd
Pavel Moskalevich <happy.proger@gmail.com>
2012-12-02 17:37:45 +0400

    Fixed tests for corpus reading.

--- Moskalevich/pylm/corpus.py
+++ Moskalevich/pylm/corpus.py
@@ -1,7 +1,7 @@
 __author__ = 'Pavel Moskalevich'
 
 from fnmatch import fnmatch
-from os.path import isdir
+from os import path
 from os import walk
 
 class Reader:
@@ -9,18 +9,18 @@ class Reader:
     read one by one.
     '''
 
-    def __init__(self, path, pattern = '*.txt'):
+    def __init__(self, folder, pattern = '*.txt'):
         self.files = []
-        self.add_files(path, pattern)
+        self.add_files(folder, pattern)
 
-    def add_files(self, path, pattern):
-        if not isdir(path):
+    def add_files(self, folder, pattern):
+        if not path.isdir(folder):
             return
-        for root, dirs, files in walk(path):
+        for root, dirs, files in walk(folder):
             matched_files = filter(lambda x: fnmatch(x, pattern), files)
             matched_files = map(lambda x: path.join(root, x), matched_files)
             self.files.extend(matched_files)
 
     def __iter__(self):
-        return self.files
+        return self.files.__iter__()
 
--- Moskalevich/pylm/test_corpus.py
+++ Moskalevich/pylm/test_corpus.py
@@ -1,10 +1,22 @@
 __author__ = 'Pavel Moskalevich'
 
-#if __name__ == "__main__" and __package__ is None:
-#    __package__ = "pylm.test.test_corpus"
-
+import unittest
 from corpus import Reader
 
-reader = Reader("test\data", "*.txt")
-for file in reader:
-    print file
+class TestReader(unittest.TestCase):
+#    def setUp(self):
+#        pass
+
+    def test_read(self):
+#        tc = unittest.TestCase()
+        reader = Reader("test\\data", "*.txt")
+        for file in reader:
+            print file
+        self.assertIn('test\\data\\hobbit.txt', reader)
+        self.assertIn('test\\data\\subdir\\dorian_gray.txt', reader)
+        self.assertIn('test\\data\\subdir\\tom_sawyer.txt', reader)
+        self.assertNotIn('test\\data\\garbage.log', reader)
+        self.assertNotIn('test\\data\\subdir\\just_txt', reader)
+
+if __name__ == '__main__':
+    unittest.main()
