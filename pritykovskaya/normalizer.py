c41ecc046ee875321132743ebdf4ba3dc12b3af6
Natasha <pritykovskaya@gmail.com>
2012-12-02 18:27:49 +0400

    remove temporary files from normalizer

--- pritykovskaya/normalizer.py
+++ pritykovskaya/normalizer.py
@@ -1,8 +1,9 @@
 # coding=utf-8
+import os
 import subprocess
 from subprocess import Popen, PIPE, STDOUT
 
-__NORMALIZER_SCRIPT = "normalizer.sh"
+__NORMALIZER_SCRIPT = "./normalizer.sh"
 
 def normalize_tag(tag):
     cmd = 'echo '+ tag + '|' + __NORMALIZER_SCRIPT
@@ -13,15 +14,17 @@ def normalize_bag_of_words(bag_of_words):
     FILE_FOR_NORMALIZER = "file_for_normalizer"
     FILE_FROM_NORMALIZER = "file_from_normalizer"
 
-    file_for_normalizer = open(FILE_FOR_NORMALIZER, "w")
-    for word in bag_of_words:
-        file_for_normalizer.write(word.encode('utf-8') + "\n")
-    file_for_normalizer.close()
+    with open(FILE_FOR_NORMALIZER, "w") as file:
+        for word in bag_of_words:
+            file.write(word.encode('utf-8') + "\n")
 
     subprocess.call('cat '+ FILE_FOR_NORMALIZER + '|' + __NORMALIZER_SCRIPT + '>' + FILE_FROM_NORMALIZER, shell = True)
 
     # with закроет файл даже если упадет исключение
     with open(FILE_FROM_NORMALIZER, "r") as file:
         result = map(str.strip, file.readlines())
+
+    os.remove(FILE_FOR_NORMALIZER)
+    os.remove(FILE_FROM_NORMALIZER)
 
     return result
