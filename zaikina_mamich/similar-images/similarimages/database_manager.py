026a5b7e9599d78c01a63b1ac8793aa3f01851ac
Ksenia Mamich <fenice@yandex-team.ru>
2012-12-02 16:15:06 +0400

    Var naming error fixed

--- zaikina_mamich/similar-images/similarimages/database_manager.py
+++ zaikina_mamich/similar-images/similarimages/database_manager.py
@@ -77,9 +77,9 @@ class DatabaseManager:
     # Calculate image parameters & save to database
     image_parameters = ImgStatisticCounter(path=cls.__path_to_image(id=img.id, name=img.name))
     img.main_colors = str(image_parameters.main_colors),
-    img.exp_value = str(image_parameters.expectation_value),
+    img.expectation_value = str(image_parameters.expectation_value),
     img.dispersion = str(image_parameters.dispersion),
-    img.std_dev = str(image_parameters.standard_deviation)
+    img.standard_deviation = str(image_parameters.standard_deviation)
 
     session.add(img)
     session.flush()
