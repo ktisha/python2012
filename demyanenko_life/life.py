fa04134f8259cbbe9cd1bce2315a5cc2ee7bb336
Ilya Demyanenko <belk94@gmail.com>
2012-12-07 01:22:22 +0400

    Реализован полнофункциональный класс поля с поиском предыдущих состояний

--- demyanenko_life/life.py
+++ demyanenko_life/life.py
@@ -1,7 +1,6 @@
 # coding: utf-8
 
 import math
-import time
 
 class field:
 
@@ -12,80 +11,79 @@ class field:
         self.__currIter = 0
         self.__cache = {}
         self.__cache[0] = startpos
-        self.__transitions = {}
-        self.__fillTransitions()
-    
-    def __fillTransitions(self):
-        try:
-            with open("dict", 'r') as f:
-                for i in xrange(0, 2 ** 16):
-                    s = f.readline().replace('\n', '')
-                    s = [int(a) for a in s.split(' ')]
-                    self.__transitions[i] = ((s[0], s[1]),
-                                             (s[2], s[3]))
-        except:
-            with open("dict", 'w') as f:
-                getBit = lambda value, number: (value >> number) % 2
-                def check(cell, neib):
-                    if cell == 0:
-                        if neib == 3:
-                            return 1
-                        else:
-                            return  0
-                    else:
-                        if 2 <= neib <= 3:
-                            return 1
-                        else:
-                            return 0
+        self.__minIter = 0
 
-                for i in xrange(0, 2 ** 16):
-                    index = (((getBit(i, 0 )), (getBit(i, 1 )), (getBit(i, 2 )), (getBit(i, 3 ))),
-                             ((getBit(i, 4 )), (getBit(i, 5 )), (getBit(i, 6 )), (getBit(i, 7 ))),
-                             ((getBit(i, 8 )), (getBit(i, 9 )), (getBit(i, 10)), (getBit(i, 11))),
-                             ((getBit(i, 12)), (getBit(i, 13)), (getBit(i, 14)), (getBit(i, 15))))
-        
-                    lt = index[1][1]
-                    ltNeib = index[0][0] + index[0][1] + index[0][2] + \
-                             index[1][0] +               index[1][2] + \
-                             index[2][0] + index[2][1] + index[2][2]
-                    lt = check(lt, ltNeib)
-        
-                    rt = index[1][2]
-                    rtNeib = index[0][1] + index[0][2] + index[0][3] +\
-                             index[1][1] +               index[1][3] +\
-                             index[2][1] + index[2][2] + index[2][3]
-                    rt = check(rt, rtNeib)
-        
-                    lb = index[2][1]
-                    lbNeib = index[1][0] + index[1][1] + index[1][2] +\
-                             index[2][0] +               index[2][2] +\
-                             index[3][0] + index[3][1] + index[3][2]
-                    lb = check(lb, lbNeib)
-        
-                    rb = index[2][2]
-                    rbNeib = index[1][1] + index[1][2] + index[1][3] +\
-                             index[2][1] +               index[2][3] +\
-                             index[3][1] + index[3][2] + index[3][3]
-                    rb = check(rb, rbNeib)
-        
-                    self.__transitions[i] = ((lt, rt),
-                                             (lb, rb))
-                    f.write("%i %i %i %i\n" % (lt, rt, lb, rb))
+    def __checkCell(self, list, i, j):
+        h = len(list)
+        w = len(list[0])
+        get = lambda i, j: list[i % h][j % w]
+        s = get(i-1, j-1) + get(i-1, j) + get(i-1, j+1) +\
+            get(i  , j-1) +               get(i  , j+1) +\
+            get(i+1, j-1) + get(i+1, j) + get(i+1, j+1)
+        if s == 3:
+            return 1
+        elif s == 2:
+            return get(i, j)
+        else:
+            return 0
 
-    def __generateUnhashed(self, num):
+    def findPrev(self, num):
+        target = self.__cache[num]
 
-        # start = time.clock()
+        def checkLine(lineNum):
+            fits = True
+            for l in xrange(self.__w):
+                if self.__checkCell(cand, lineNum, l) != target[lineNum][l]:
+                    fits = False
+                    break
+            return fits
 
-        if num < 0:
+        def generateLists(len):
+            for num in xrange(2 ** len):
+                line = []
+                temp = num
+                for i in xrange(len):
+                    line.append(temp & 1)
+                    temp >>= 1
+                yield line
+
+        oldCands = []
+        newCands = []
+
+        for i in generateLists(self.__w):
+            for j in generateLists(self.__w):
+                for k in generateLists(self.__w):
+                    cand = [list(i), list(j), list(k)]
+                    if checkLine(1):
+                        newCands.append(cand)
+
+        for i in xrange(3, self.__h):
+            oldCands = newCands
+            newCands = []
+
+            for j in oldCands:
+                for k in generateLists(self.__w):
+                    cand = list(j)
+                    cand.append(list(k))
+                    if checkLine(i - 1):
+                        newCands.append(cand)
+
+        res = []
+        for cand in newCands:
+            if checkLine(self.__h - 1) and checkLine(0):
+                res.append(cand)
+
+        return res
+
+
+    def __generate(self, num):
+        if num < self.__minIter:
             raise ValueError
 
-        bestKey = 0
-
-        for key in self.__cache.keys():
-            if key > bestKey:
-                bestKey = key
-            else:
-                break
+        bestKey = num
+        keys = self.__cache.keys()
+        while not (bestKey in keys):
+            bestKey -= 1
 
         curr = self.__cache[bestKey]
 
@@ -97,79 +95,20 @@ class field:
                 res += index[int(math.floor(i / 4))][i % 4] << i
             return res
 
-        def check(cell, neib):
-            if cell == 0:
-                if neib == 3:
-                    return 1
-                else:
-                    return  0
-            else:
-                if 2 <= neib <= 3:
-                    return 1
-                else:
-                    return 0
-
         for step in xrange(bestKey, num):
-            if step % 100 == 0:
-                print step
             dest = [[]] * self.__h
             for i in xrange(0, self.__h):
                 dest [i] = [0] * self.__w
 
             for i in xrange(0, self.__h):
                 for j in xrange(0, self.__w):
-                    neib = get(i-1, j-1) + get(i-1, j  ) + get(i-1, j+1) +\
-                           get(i  , j-1) +                 get(i  , j+1) +\
-                           get(i+1, j-1) + get(i+1, j  ) + get(i+1, j+1)
-                    dest[i][j] = check(get(i, j), neib)
+                    dest[i][j] = self.__checkCell(curr, i, j)
+
             curr = dest
             self.__cache[step] = curr
 
-        # print "Unhashed: %f" % (time.clock() - start)
         return curr
 
-    def __generate(self, num):
-        # start = time.clock()
-        if num < 0:
-            raise ValueError
-
-        bestKey = 0
-
-        for key in self.__cache.keys():
-            if key > bestKey:
-                bestKey = key
-            else:
-                break
-
-        curr = self.__cache[bestKey]
-
-        get = lambda i, j: curr[i % self.__h][j % self.__w]
-
-        def getNum(index):
-            res = 0
-            for i in xrange(0, 16):
-                res += index[int(math.floor(i / 4))][i % 4] << i
-            return res
-
-        for step in xrange(bestKey, num):
-            dest = [[]] * self.__h
-            for i in xrange(0, self.__h):
-                dest [i] = [0] * self.__w
-
-            for i in xrange(0, self.__h, 2):
-                for j in xrange(0, self.__w, 2):
-                    index = getNum(((get(i-1, j-1), get(i-1, j  ), get(i-1, j+1), get(i-1, j+2)),
-                                    (get(i  , j-1), get(i  , j  ), get(i  , j+1), get(i  , j+2)),
-                                    (get(i+1, j-1), get(i+1, j  ), get(i+1, j+1), get(i+1, j+2)),
-                                    (get(i+2, j-1), get(i+2, j  ), get(i+2, j+1), get(i+2, j+2))))
-                    dest[(i  ) % self.__h][(j  ) % self.__w] = self.__transitions[index][0][0]
-                    dest[(i  ) % self.__h][(j+1) % self.__w] = self.__transitions[index][0][1]
-                    dest[(i+1) % self.__h][(j  ) % self.__w] = self.__transitions[index][1][0]
-                    dest[(i+1) % self.__h][(j+1) % self.__w] = self.__transitions[index][1][1]
-            curr = dest
-
-        # print "Hashed: %f" % (time.clock() - start)
-        return curr
 
 
     def move(self, delta):
@@ -201,9 +140,7 @@ class field:
                 self.__cache.pop(key)
         for index in indexes:
             if not self.__cache.has_key(index):
-                print "cache miss: %i" % index
-                # self.__cache[index] = self.__generate(index)
-                self.__cache[index] = self.__generateUnhashed(index)
+                self.__cache[index] = self.__generate(index)
 
     def getField(self):
         return self.__cache[self.__currIter]
@@ -212,44 +149,107 @@ class field:
         return self.__currIter
 
 
+def replace(i):
+    if i == 1:
+        return 'O'
+    else:
+        return ' '
+
 def show(field):
-    print field.getCurrIter()
+    #print field.getCurrIter()
     data = field.getField()
     print '----------'
     for row in data:
-        s = str(row).replace(', ', '').replace('1', 'O').replace('0', ' ')[1:-1]
-        print s
+        print ''.join(map(replace, row))
+        #s = str(row).replace(', ', '').replace('1', 'O').replace('0', ' ')[1:-1]
+        #print s
+        pass
     print '----------'
 
-start = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
-         [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
-#start = [[0, 1, 0, 0, 0],
-#         [0, 0, 1, 0, 0],
-#         [1, 1, 1, 0, 0],
-#         [0, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 0]]
-f = field(start)
+gun = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+       [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+
+glider = [[0, 0, 0, 0, 0],
+          [0, 0, 1, 0, 0],
+          [0, 0, 0, 1, 0],
+          [0, 1, 1, 1, 0],
+          [0, 0, 0, 0, 0]]
+
+glider2 = [[0, 0, 0, 0, 0],
+           [0, 1, 0, 0, 0],
+           [0, 0, 1, 1, 0],
+           [0, 1, 1, 0, 0],
+           [0, 0, 0, 0, 0]]
+
+glider3 = [[0, 0, 0, 0, 0],
+           [0, 0, 0, 1, 0],
+           [0, 1, 0, 1, 0],
+           [0, 0, 1, 1, 0],
+           [0, 0, 0, 0, 0]]
+
+r = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+small = [[0, 0, 0, 0],
+         [0, 1, 1, 0],
+         [0, 1, 1, 0],
+         [0, 0, 0, 0]]
+
+f = field(glider3)
 show(f)
-while True:
-    raw_input()
-    f.move(100000)
-    show(f)
+raw_input()
+prevs = f.findPrev(0)
+if len(prevs) == 0:
+    print "Fail"
+else:
+    for res in prevs:
+        print '----------'
+        for row in res:
+            print ''.join(map(replace, row))
+            pass
+        print '----------'
+        print
+raw_input()
+
+#while True:
+#    raw_input()
+#    f.move(100)
+#    show(f)
