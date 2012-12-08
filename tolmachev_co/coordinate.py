6de1dcadd081791bed0ca8fb0c0651572a86bf82
Alexander Tolmachev <starlight@yandex-team.ru>
2012-12-09 00:33:58 +0400

    Pair class is added

---
+++ tolmachev_co/coordinate.py
@@ -0,0 +1,17 @@
+class Coordinate:
+    __x = 0
+    __y = 0
+
+    def __init__(self, x, y):
+        self.__x = x
+        self.__y = y
+
+    def get_x(self):
+        return self.__x
+
+    def get_y(self):
+        return self.__y
+
+    def __hash__(self):
+        return (self.__x, self.__y).__hash__()
+
