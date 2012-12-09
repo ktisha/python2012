2b57e8163dbb4f3caf531ab37c864318df347174
Alexander Tolmachev <starlight@yandex-team.ru>
2012-12-09 22:13:13 +0400

    __eq__ and __cmp__ methods are aaded to Coordinate class

--- tolmachev_co/map/coordinate.py
+++ tolmachev_co/map/coordinate.py
@@ -12,5 +12,22 @@ class Coordinate:
     def __hash__(self):
         return (self.__x, self.__y).__hash__()
 
-    
+    def __eq__(self, other):
+        if not other:
+            return False
+        elif not isinstance(other, Coordinate):
+            return False
+        return self.get_x() == other.get_x() and self.get_y() == other.get_y()
 
+    def __cmp__(self, other):
+        if self.get_x() < other.get_x():
+            return -1
+        elif self.get_x() == other.get_x():
+            if self.get_y() < other.get_y():
+                return -1
+            elif self.get_y() == other.get_y():
+                return 0
+            else:
+                return 1
+        else:
+            return 1
