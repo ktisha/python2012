0648799246911275438ef093820cbef431fd1399
Alexander Tolmachev <starlight@yandex-team.ru>
2012-12-09 18:56:05 +0400

    Beggar states are added

--- tolmachev_co/actors/beggar.py
+++ tolmachev_co/actors/beggar.py
@@ -1,5 +1,13 @@
 from actors.actor import Actor
 
+class BeggarState:
+    IS_SEARCHING_A_BOTTLE = 1
+    IS_WALKING_WITH_BOTTLE = 2
+    IS_IN_POINT_FOR_GLASS = 3
+
 class Beggar (Actor):
+    def __init__(self):
+        self.__current_state = BeggarState.IS_SEARCHING_A_BOTTLE
+
     def accept_visitor(self, visitor):
         visitor.visit_beggar(self)
