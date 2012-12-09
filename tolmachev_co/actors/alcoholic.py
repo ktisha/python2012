886d62af797d3fb035cf624a1013bf326743f6bf
Alexander Tolmachev <starlight@yandex-team.ru>
2012-12-09 18:32:50 +0400

    Alcoholic states are added

--- tolmachev_co/actors/alcoholic.py
+++ tolmachev_co/actors/alcoholic.py
@@ -1,5 +1,20 @@
 from actors.actor import Actor
 
+class AlcoholicState:
+    IS_AWAKE = 1
+    IS_SLEEPING = 2
+    IS_CAUGHT_BY_POLICEMAN = 3
+
 class Alcoholic (Actor):
+    def __init__(self):
+        self.__current_state = AlcoholicState.IS_AWAKE
+        self.__has_bottle = True
+
     def accept_visitor(self, visitor):
         visitor.visit_alcoholic(self)
+
+    def get_current_state(self):
+        return self.__current_state
+
+    def has_bottle(self):
+        return self.__has_bottle
