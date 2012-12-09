a3da226d4b851909d5f0c5e9e4f4e7f9ff41a175
Alexander Tolmachev <starlight@yandex-team.ru>
2012-12-09 19:26:53 +0400

    Names of actors states are changed

--- tolmachev_co/actors/alcoholic.py
+++ tolmachev_co/actors/alcoholic.py
@@ -1,26 +1,26 @@
 from actors.actor import Actor
 
 class AlcoholicState:
-    IS_AWAKE = 1
-    IS_SLEEPING = 2
-    IS_CAUGHT_BY_POLICEMAN = 3
+    AWAKE = 1
+    SLEEPING = 2
+    CAUGHT_BY_POLICEMAN = 3
 
 class Alcoholic (Actor):
     def __init__(self):
-        self.__current_state = AlcoholicState.IS_AWAKE
+        self.__current_state = AlcoholicState.AWAKE
         self.__has_bottle = True
 
     def accept_visitor(self, visitor):
         visitor.visit_alcoholic(self)
 
     def is_awake(self):
-        return self.__current_state == AlcoholicState.IS_AWAKE
+        return self.__current_state == AlcoholicState.AWAKE
 
     def is_sleeping(self):
-        return self.__current_state == AlcoholicState.IS_SLEEPING
+        return self.__current_state == AlcoholicState.SLEEPING
 
     def is_caught_by_policeman(self):
-        return self.__current_state == AlcoholicState.IS_CAUGHT_BY_POLICEMAN
+        return self.__current_state == AlcoholicState.CAUGHT_BY_POLICEMAN
 
     def has_bottle(self):
         return self.__has_bottle
--- tolmachev_co/actors/beggar.py
+++ tolmachev_co/actors/beggar.py
@@ -1,22 +1,22 @@
 from actors.actor import Actor
 
 class BeggarState:
-    IS_SEARCHING_A_BOTTLE = 1
-    IS_WALKING_WITH_A_BOTTLE = 2
-    IS_IN_TAVERN = 3
+    SEARCHING_A_BOTTLE = 1
+    WALKING_WITH_A_BOTTLE = 2
+    AT_THE_TAVERN = 3
 
 class Beggar (Actor):
     def __init__(self):
-        self.__current_state = BeggarState.IS_SEARCHING_A_BOTTLE
+        self.__current_state = BeggarState.SEARCHING_A_BOTTLE
 
     def accept_visitor(self, visitor):
         visitor.visit_beggar(self)
 
     def is_searching_a_bottle(self):
-        return self.__current_state == BeggarState.IS_SEARCHING_A_BOTTLE
+        return self.__current_state == BeggarState.SEARCHING_A_BOTTLE
 
     def is_walking_with_a_bottle(self):
-        return self.__current_state == BeggarState.IS_WALKING_WITH_A_BOTTLE
+        return self.__current_state == BeggarState.WALKING_WITH_A_BOTTLE
 
     def is_in_tavern(self):
-        return self.__current_state == BeggarState.IS_IN_TAVERN
+        return self.__current_state == BeggarState.AT_THE_TAVERN
